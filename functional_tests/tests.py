eec187a0581ca18624ea08db5d743b8d97845fe3
Sunwoo Lee <eldkqmfhf@gmail.com>
2016-09-12 16:45:02 +0900

    두명의 사용자가 독립적으로 사용하도록 FT 추가

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,7 +33,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('공작깃털 사기')
         inputbox.send_keys(Keys.ENTER)
-
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
 
@@ -41,9 +42,41 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
+
+        # 새로은 사용자인 프란시스가 사이트에 접속한다
+
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다 #
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+
+        # 프란시스가 새로운 작업아이템을 입력하기 시작한다
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, 'list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertIn('우유 사기', page_text)
 
         self.fail('finished the test!')
 
