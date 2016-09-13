de034e659b7e5f7c4928a861bfe4286fbfc71c85
Sunwoo Lee <eldkqmfhf@gmail.com>
2016-09-14 00:35:31 +0900

    모델을 조정해서 아이템들이 다른 목록과 연계되도록 수정

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,23 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+
+        self.assertEqual(response.content.decode(), expected_html)
+
 
 class ListViewTest(TestCase):
 
@@ -12,8 +28,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -38,38 +55,30 @@ class ListViewTest(TestCase):
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-
-        self.assertEqual(response.content.decode(), expected_html)
-
-
-
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
-        saved_item = Item.objects.all()
-        self.assertEqual(saved_item.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_item[0]
-        second_saved_item = saved_item[1]
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
