6f0796425ea54450a80d272887e9b972df6e585b
Sunwoo Lee <eldkqmfhf@gmail.com>
2016-09-16 18:37:36 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -34,7 +35,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
         )
-        
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get(self.live_server_url)
